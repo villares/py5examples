b015a00cbd7af6977692ec38bc02d5c4cbdd826c
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-08-18 20:51:42 -0300

    Update IntAndFloat.py

--- examples-from-Processing-Python-mode/Python Mode Differences/IntAndFloat/IntAndFloat.py
+++ examples-from-Processing-Python-mode/Python Mode Differences/IntAndFloat/IntAndFloat.py
@@ -1,5 +1,4 @@
-# The int() and float() functions in Python mode are those
-# provided by Python, and they work differently from the
+# The int() and float() functions in Python work differently from the
 # ones you may know from Processing's Java mode.
 
 # Some uses are the same:
@@ -17,14 +16,11 @@ assert ints == [12, 47, 3]
 # It's easy to build a function that converts something to
 # an int, and providea a default value when there's no
 # sensible conversion.
-
-
 def to_int(x, default=0):
     try:
         return int(x)
     except ValueError:
         return default
-
 
 assert to_int('12') == 12
 assert to_int('banana') == 0
