41c0f71e7116212d918d0255416d9532d831fded
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-08-21 15:42:57 -0300

    Update Milliseconds.py

--- examples-from-Processing-Python-mode/Basics/Input/Milliseconds/Milliseconds.py
+++ examples-from-Processing-Python-mode/Basics/Input/Milliseconds/Milliseconds.py
@@ -9,14 +9,14 @@ different patterns in time are created.
 
 
 def setup():
-    global scale
+    global scale_factor
     size(640, 360)
     no_stroke()
-    scale = width / 20
+    scale_factor = width // 20
 
 
 def draw():
-    for i in range(0, scale, 1):
-        color_mode(RGB, (i + 1) * scale * 10)
-        fill(millis() % ((i + 1) * scale * 10))
-        rect(i * scale, 0, scale, height)
+    for i in range(0, scale_factor, 1):
+        color_mode(RGB, (i + 1) * scale_factor * 10)
+        fill(millis() % ((i + 1) * scale_factor * 10))
+        rect(i * scale_factor, 0, scale_factor, height)
