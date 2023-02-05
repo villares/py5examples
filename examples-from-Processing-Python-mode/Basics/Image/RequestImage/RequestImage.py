63f105b4c260af33ef3a5475871d024b9a0182a7
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-02-05 12:57:25 -0300

    kind of hard... bit works!  request_image provides a Py5Promise object...

--- examples-from-Processing-Python-mode/Basics/Image/RequestImage/RequestImage.py
+++ examples-from-Processing-Python-mode/Basics/Image/RequestImage/RequestImage.py
@@ -10,56 +10,50 @@
  * These images are small for a quick download, but try it with your own huge
  * images to get the full effect.
  """
+
 img_count = 12
 
-imgs = []
+image_promises = []
 
 # Keeps track of loaded images (True or False)
 load_states = []
-
 
 def setup():
     size(640, 360)
     smooth()
     # Load images asynchronously
     for i in range(img_count):
-        imgs.append(request_image("PT_anim" + nf(i, 4) + ".gif"))
+        image_promises.append(request_image(Path('images') / f'PT_anim{i:04}.gif'))
         load_states.append(False)
 
 
 def draw():
     background(0)
-
     # Start loading animation
     run_loader_ani()
-
-    for i, img in enumerate(imgs):
+    for i, img in enumerate(image_promises):
         # Check if individual images are fully loaded
-        if img.width not in (0, -1):
+        if img.is_ready:
             # As images are loaded set True in boolean array
             load_states[i] = True
-
     # When all images are loaded draw them to the screen
     if all(load_states):
         draw_images()
 
 
 def draw_images():
-    y = (height - imgs[0].height) / 2
-    img_width = width / len(imgs)
-    for i, img in enumerate(imgs):
+    y = (height - image_promises[0].result.height) / 2
+    img_width = width / len(image_promises)
+    for i, img_promise in enumerate(image_promises):
+        img = img_promise.result
         image(img, img_width * i, y, img.width, img.height)
 
 # Loading animation
-
-
 loader_x, loader_y = 0, 0
 theta = 0
 
-
 def run_loader_ani():
     global loader_x, loader_y, theta
-
     # Only run when images are loading
     if not all(load_states):
         ellipse(loader_x, loader_y, 10, 10)
