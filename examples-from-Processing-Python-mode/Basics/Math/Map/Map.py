10c9228e7dda6829adf065058acebd2494d6c895
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-02-05 11:41:54 -0300

    Update Map.py

--- examples-from-Processing-Python-mode/Basics/Math/Map/Map.py
+++ examples-from-Processing-Python-mode/Basics/Math/Map/Map.py
@@ -1,7 +1,7 @@
 '''
  Map.
 
- Use the map() function to take any number and scale it to a new number
+ Use the remap() function to take any number and scale it to a new number
  that is more useful for the project that you are working on. For example, use the
  numbers from the mouse position to control the size or color of a shape.
  In this example, the mouse’s x-coordinate (a number between 0 and 360) is scaled to a
@@ -21,10 +21,10 @@ def draw():
     background(0)
 
     # Scale the mouseX value from 0 to 640 to a range between 0 and 175
-    c = map(mouse_x, 0, width, 0, 175)
+    c = remap(mouse_x, 0, width, 0, 175)
 
     # Scale the mouseX value from 0 to 640 to a range between 40 and 300
-    d = map(mouse_x, 0, width, 40, 300)
+    d = remap(mouse_x, 0, width, 40, 300)
 
     fill(255, c, 0)
     ellipse(width / 2, height / 2, d, d)
